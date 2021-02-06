4521fb1f00b26b2c1a56b107cd9beb9f34ad6f98
unknown <aymeric.aveline@gmail.com>
2021-02-06 10:05:49 +0100

    Merge various commits

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM alpine:3.7
+FROM karnalab/karna:v2.0.0
+
 LABEL "author"="Aymeric Aveline"
 LABEL "com.github.actions.name"="Karna action"
 LABEL "com.github.actions.description"="Deploy your Lambda functions with Karna"
@@ -6,11 +7,6 @@ LABEL "com.github.actions.icon"="upload-cloud"
 LABEL "com.github.actions.color"="green"
 LABEL version="2.0.0"
 
-# Set the Current Working Directory inside the container
-WORKDIR /app
+COPY entrypoint.sh /entrypoint.sh
 
-RUN apk --no-cache add curl
-
-COPY entrypoint.sh /app/entrypoint.sh
-
-ENTRYPOINT [ "sh", "/app/entrypoint.sh" ]
+ENTRYPOINT ["sh", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,4 @@
-#!bin/sh
-if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
-	echo "⚠️ Set the AWS_ACCESS_KEY_ID env variable"
-	exit 1
-fi
-
-if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
-	echo "⚠️ Set the AWS_SECRET_ACCESS_KEY secret"
-	exit 1
-fi
-
-if [[ -z "$AWS_REGION" ]]; then
-	echo "⚠️ Set the AWS_REGION env variable"
-	exit 1
-fi
-
-curl -s https://api.github.com/repos/KarnaLab/karna/releases/latest \
-| grep "browser_download_url.karna-linux-amd64\.tar\.gz" \
-| cut -d ":" -f 2,3 \
-| tr -d \" \
-| wget -qi -
-
-tarball="$(find . -name "karna-linux-amd64.tar.gz")"
-tar -xzf $tarball
-
-chmod +x karna
-
-mv karna $GITHUB_WORKSPACE
-# CD into workspace 
-cd "$GITHUB_WORKSPACE"
+#!/bin/sh
 
 # Run Karna command
-./karna deploy -a $1 -f $2
+/root/karna deploy -a $1 -f $2
