ffef7d3820b2da453d1819cab091078e3cf785b8
unknown <aymeric.aveline@gmail.com>
2021-02-06 09:14:20 +0100

    Attempt to pull directly karna executable instead of docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
-	echo "⚠️ Set the AWS_ACCESS_KEY_ID secret"
+	echo "⚠️ Set the AWS_ACCESS_KEY_ID env variable"
 	exit 1
 fi
 
@@ -10,12 +10,24 @@ if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
 fi
 
 if [[ -z "$AWS_REGION" ]]; then
-	echo "⚠️ Set the AWS_REGION secret"
+	echo "⚠️ Set the AWS_REGION env variable"
 	exit 1
 fi
 
+curl -s https://api.github.com/repos/KarnaLab/karna/releases/latest \
+| grep "browser_download_url.karna-linux-amd64\.tar\.gz" \
+| cut -d ":" -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+tarball="$(find . -name "karna-linux-amd64.tar.gz")"
+tar -xzf $tarball
+
+chmod +x karna
+
+mv karna $GITHUB_WORKSPACE
 # CD into workspace 
 cd "$GITHUB_WORKSPACE"
 
 # Run Karna command
-karna deploy -a $1 -t $2
+./karna deploy -a $1 -f $2
