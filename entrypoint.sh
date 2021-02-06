35acac0fee805a791fe2b83c61f206cf4951c1a2
unknown <aymeric.aveline@gmail.com>
2021-02-06 15:34:22 +0100

    Break if AWS env vars are not set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+	echo "⚠️ Set the AWS_ACCESS_KEY_ID secret"
+	exit 1
+fi
 
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+	echo "⚠️ Set the AWS_SECRET_ACCESS_KEY secret"
+	exit 1
+fi
+
+if [[ -z "$AWS_REGION" ]]; then
+	echo "⚠️ Set the AWS_REGION secret"
+	exit 1
+fi
 # Run Karna command
 /root/karna deploy -a $1 -f $2
